cb6c85afb2c1309d2cf30047f85ffc95ef620fd3
Mohanishkrv <61794514+Mohanishkrv@users.noreply.github.com>
2024-01-12 23:41:48 +0530

    Add files via upload

---
+++ Meteoros_Project/Data/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Meteoros_Project.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Meteoros_Project.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<CustomerProduct> CustomersProducts { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<CustomerProduct>()
+                .HasKey(cp => new { cp.CustomerId, cp.ProductId });
+
+            modelBuilder.Entity<CustomerProduct>()
+                .HasOne(cp => cp.Customer)
+                .WithMany(c => c.CustomerProducts)
+                .HasForeignKey(cp => cp.CustomerId);
+
+            modelBuilder.Entity<CustomerProduct>()
+                .HasOne(cp => cp.Product)
+                .WithMany(p => p.CustomerProducts)
+                .HasForeignKey(cp => cp.ProductId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Meteoros_Project/Migrations/20240112153433_Date Column for Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeteorosProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class DateColumnforCustomer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "Customers",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "Customers");
+        }
+    }
+}
