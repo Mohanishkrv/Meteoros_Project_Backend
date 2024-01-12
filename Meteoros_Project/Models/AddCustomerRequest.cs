a6c50ac34816bf3ae9e7427c2e9595c5051c59e6
Mohanishkrv <61794514+Mohanishkrv@users.noreply.github.com>
2024-01-12 23:26:00 +0530

    Deleting Outdated Changes

--- Meteoros_Project/Models/AddCustomerRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Meteoros_Project.Models
-{
-    public class AddCustomerRequest
-    {
-        public string Name { get; set; }
-        public string PhoneNumber { get; set; }
-    }
-}
--- Meteoros_Project/Service/ISales.cs
+++
@@ -1,9 +0,0 @@
-﻿using Meteoros_Project.Models;
-
-namespace Meteoros_Project.Service
-{
-    public interface ISales
-    {
-        int GetTotalSales(Product product);
-    }
-}
